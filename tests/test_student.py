95b38f62984ee9066f7d2a8bcdb2fbbf7e950e45
Billie Betts <lily.a.betts@gmail.com>
2022-10-05 13:46:27 -0400

    test_get_num added

--- tests/test_student.py
+++ tests/test_student.py
@@ -12,7 +12,22 @@ def test_new_student():
     # assert
     assert aminah.name == name
     assert aminah.grade == grade
-    assert amina.classes= classes
+    assert aminah.classes == classes
     assert len(aminah.classes) == 5
 
-    
+def test_get_num_classes_adjusts():
+        # arrange
+    name = "Aminah"
+    grade = "Freshman"
+    classes = ["Chemistry", "Algebra", "Cooking", "Paint", "Poetry"]
+
+    # act
+    aminah = Student(name, grade, classes)
+    aminah.add("Gym")
+
+    # assert
+    assert aminah.name == name
+    assert aminah.grade == grade
+    assert aminah.classes == classes
+    assert len(aminah.classes) == 6
+
